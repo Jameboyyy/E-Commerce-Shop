5d57265f312eb99a4840cf33fb4f9dba8e72f44d
Jameboyyy <63523469+Jameboyyy@users.noreply.github.com>
2022-09-05 16:30:44 -0700

    Updated
    Updated Book Home page and book info. Will continue in cart details

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,18 @@ import Footer from './components/Footer';
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from "./data"
+import BooksInfo from './pages/BooksInfo';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route />
           <Nav />
-          <Home />
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact render={() => <Books books={books} />} />
+          <Route path="/books/:id" render={() => <BooksInfo books={books} />} />
           <Footer />
       </div>
     </Router>
